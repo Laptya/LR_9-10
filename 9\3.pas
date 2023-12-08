41c441f803873d983cc253cab79f976e906e593b
Laptya <144488450+Laptya@users.noreply.github.com>
2023-12-08 08:04:24 +0300

    Create 9\3.pas

---
+++ 9\3.pas
@@ -0,0 +1,22 @@
+﻿program zad3;
+var
+i,n:integer;
+a: array of integer;
+function per():integer;
+var
+i:integer;
+begin
+per:=0;
+for i:=1 to n do
+if a[i-1] < 0 then begin per:=a[i-1]: end:
+end;
+
+begin
+readln (n);
+setlength (a,n);
+for i:=1 to n do
+a[i-1] := random (20)-10;
+writeln(a);
+per();
+writeln(per);
+end.
